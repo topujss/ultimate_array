72afd232e714e4110e052d06d4f33625da99dbef
topujss <adahmedt@gmail.com>
2023-05-22 14:08:57 -0400

    all about array

---
+++ Array.js
@@ -0,0 +1,27 @@
+let langs = ['java', 'javascript', 'rust'];
+
+/** Method
+ * join them together
+ * langs.join(' ') or
+ * langs.toString() <= but this doesn't have separator;
+ */
+
+/**
+ * To know the length
+ * langs.length
+ */
+
+/**
+ * To remove the last item from the array 
+ * return the value that removed
+ * langs.pop()
+ */
+
+/**
+ * To add the last item from the array
+ * return number of items added
+ * langs.pop()
+ */
+
+let arr = langs.push('python');
+console.log(arr);
